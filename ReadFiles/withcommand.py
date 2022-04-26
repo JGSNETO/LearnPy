966ee9429c92ce6b7aa233fb7f67448caeaeb810
José Neto <jgsnto.sd@gmail.com>
2022-04-26 13:10:03 -0300

    How to use with open

---
+++ ReadFiles/withcommand.py
@@ -0,0 +1,9 @@
+#Uma coisa fundamental é lembrarmos de fechar o arquivo se não ao saírmos do interpretador ou do programa
+#Em execução iremos perder todas as escritas feitas no arquivo. Para não corrermos esse risco podemos usar o comando
+#with
+#Argumento w: Sempre irá apagar o arquivo existente para que comecemos a escrever a partir do zero
+#Argumento a: Inserir sem apagar o que já tem
+#Argumento wb: Escrever em forma binária
+
+#With open (path, 'w') as arq:
+#   suite
